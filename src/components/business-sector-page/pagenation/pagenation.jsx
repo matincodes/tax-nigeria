b5894ad7778a2dd9db7d179489e0be8e37873d03
Egbetokun Timilehin <timmyaof02@gmail.com>
2024-05-24 14:25:04 +0100

    finished

---
+++ src/components/business-sector-page/pagenation/pagenation.jsx
@@ -0,0 +1,12 @@
+// const pagenation = () => {
+//   return (
+//     <div className="border p-2 mt-3 flex font-manrope">
+//           <ul className="flex gap-4">
+//                <button className="border text-[14px]"> Previous Page </button>
+//                <button className="border w-[30px] h-[30px] rounded-full flex place-content-center items-center"> <li className="font-semibold text-[14px]"> 1 </li> </button>
+//           </ul>
+//      </div>
+//   )
+// }
+
+// export default pagenation
