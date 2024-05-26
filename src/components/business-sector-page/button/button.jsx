460150c8a8b3abda12d13a63cb2b5eb6b03621f1
Fawaz Adeniji <fawazadeniji123@gmail.com>
2024-05-26 10:56:31 +0100

    refactor: Update button component to use consistent text size

--- src/components/business-sector-page/button/button.jsx
+++ src/components/business-sector-page/button/button.jsx
@@ -4,11 +4,11 @@ const Button = (props) =>{
        <div>
          <button type="button" className="border rounded-lg pl-[25px] pr-[25px] pt-[15px] pb-[15px] h-[40px] bg-tax-blue text-tax-gray flex gap-2 text-center place-content-center items-center"> 
            {(props.icon && (props.iconposition === "left")) ? (
-             <><h2 className="text-[19px]">{props.icon}</h2> <p className="text-md">{props.text}</p></>
+             <><h2 className="text-[19px]">{props.icon}</h2> <p className="text-sm">{props.text}</p></>
            ) : (props.icon && props.iconposition === "right") ? (
-             <><p className="text-md">{props.text}</p> <h2 className="text-[19px]">{props.icon}</h2></>
+             <><p className="text-sm">{props.text}</p> <h2 className="text-[19px]">{props.icon}</h2></>
            ) : (
-             <p className="text-md">{props.text}</p>
+             <p className="text-sm">{props.text}</p>
            )}
           </button>
              
