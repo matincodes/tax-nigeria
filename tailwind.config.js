db752ad6d43016595829b2f711f74dc056a6c070
Fawaz Adeniji <fawazadeniji123@gmail.com>
2024-05-26 13:24:00 +0100

    feat: configured shad cn and add select, table, and pagination component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,48 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
+  darkMode: ['class'],
+  content: [
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+    './app/**/*.{js,jsx}',
+    './src/**/*.{js,jsx}',
+  ],
+  prefix: '',
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
     extend: {
-      colors:{
-        "tax-gray": "#F2F2F2",
-        "tax-lime": "#B0C892",
-        "tax-blue": "#4E72D1",
-        "text-gray": "#808080"
+      colors: {
+        'tax-gray': '#F2F2F2',
+        'tax-lime': '#B0C892',
+        'tax-blue': '#4E72D1',
+        'text-gray': '#808080',
       },
       fontFamily: {
-        "manrope": ['Manrope', "sans-serif"],
-        "montserrat": ['Montserrat', "sans-serif"],
-        "poppins": ['Poppins', "sans-serif"]
-      }
+        manrope: ['Manrope', 'sans-serif'],
+        montserrat: ['Montserrat', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
     },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animate')],
 }
-
