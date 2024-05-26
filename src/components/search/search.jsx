81b0df08057bc562783ae73cbf559ed102db36bd
Fawaz Adeniji <fawazadeniji123@gmail.com>
2024-05-26 15:06:50 +0100

    refactor: Update class attribute to className in search component

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -2,18 +2,14 @@ import search from '../../assets/img/search.svg'
 
 const Search = () => {
   return (
-    <div class='relative w-1/3'>
-      <div class='absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none'>
-        <img
-          src={search}
-          alt='search'
-          className='w-4 h-4 text-gray-500'
-        />
+    <div className='relative w-1/3'>
+      <div className='absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none'>
+        <img src={search} alt='search' className='w-4 h-4 text-gray-500' />
       </div>
       <input
         type='search'
         id='default-search'
-        class='block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50'
+        className='block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50'
         placeholder='Search by Name/ID'
       />
     </div>
