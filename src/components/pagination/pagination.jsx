8f6e10e88e2beaec6342a1f0ae6109df841af6c1
Fawaz Adeniji <fawazadeniji123@gmail.com>
2024-07-23 07:04:58 +0100

    feat: Improve pagination component
    This commit improves the pagination component by updating the JSX code to use more semantic HTML elements and applying consistent styling. The buttons for previous and next pages are now disabled when the first or last page is reached, respectively. Additionally, the pagination now displays the current page number in a highlighted style. These changes enhance the usability and accessibility of the pagination component.

--- src/components/pagination/pagination.jsx
+++ src/components/pagination/pagination.jsx
@@ -2,51 +2,54 @@ import React from 'react'
 
 function pagination({paginationArray, paginationSection, prevPage, nextPage}) {
   return (
+    <div className='p-2 mt-3 flex font-manrope'>
+      <ul className='flex gap-4'>
+        <button
+          className={`${
+            paginationSection === paginationArray[0]
+              ? 'disabled text-[14px] text-[#74737371]'
+              : 'text-[14px]'
+          }`}
+          onClick={paginationSection === paginationArray[0] ? null : prevPage}
+          name='prevPage'
+          disabled={paginationSection === paginationArray[0]}
+        >
+          Previous Page
+        </button>
 
-      <div className="p-2 mt-3 flex font-manrope">
-        <ul className="flex gap-4">
+        {paginationArray.map(sector => (
           <button
             className={`${
-              paginationSection === paginationArray[0]
-                ? "disabled text-[14px] text-[#74737371]"
-                : "text-[14px]"
+              paginationSection === sector
+                ? 'w-[30px] h-[30px] rounded-full flex place-content-center items-center bg-tax-blue text-white'
+                : 'w-[30px] h-[30px] rounded-full flex place-content-center items-center'
             }`}
-            onClick={paginationSection === paginationArray[0] ? null : prevPage}
-            name="prevPage"
-            disabled={paginationSection === paginationArray[0]}
+            key={sector.id}
           >
-            Previous Page
+            <li className='font-semibold text-[14px]' id='sectorId'>
+              {sector.id + 1}
+            </li>
           </button>
+        ))}
 
-          {paginationArray.map((sector) => (
-            <button
-              className={`${
-                paginationSection === sector
-                  ? "w-[30px] h-[30px] rounded-full flex place-content-center items-center bg-tax-blue text-white"
-                  : "w-[30px] h-[30px] rounded-full flex place-content-center items-center"
-              }`}
-              key={sector.id}
-            >
-              <li
-                className="font-semibold text-[14px]"
-                id="sectorId"
-              >
-                {sector.id + 1}
-              </li>
-            </button>
-          ))}
-
-          {/* Next Page */}
-          <button
-            className="text-[14px]"
-            onClick={nextPage}
-            name="nextPage"
-          >
-            Next Page
-          </button>
-          {/* Next Page */}
-        </ul>
-      </div>
+        {/* Next Page */}
+        <button
+          className={`${
+            paginationSection === paginationArray.at(-1)
+              ? 'disabled text-[14px] text-[#74737371]'
+              : 'text-[14px]'
+          }`}
+          onClick={
+            paginationSection === paginationArray.at(-1) ? null : nextPage
+          }
+          disabled={paginationSection === paginationArray.at(-1)}
+          name='nextPage'
+        >
+          Next Page
+        </button>
+        {/* Next Page */}
+      </ul>
+    </div>
   )
 }
 
