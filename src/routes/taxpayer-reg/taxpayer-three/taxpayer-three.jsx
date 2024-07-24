49d5d5d87fb5cf905aa47aa0c9da97ac473eb596
Fawaz Adeniji <fawazadeniji123@gmail.com>
2024-07-24 07:40:46 +0100

    refactor: Update TaxpayerForm component with associations and business types
    This commit updates the TaxpayerForm component to fetch associations and business types from an API and populate the corresponding select options. This improves the functionality and user experience of the form.

--- src/routes/taxpayer-reg/taxpayer-three/taxpayer-three.jsx
+++ src/routes/taxpayer-reg/taxpayer-three/taxpayer-three.jsx
@@ -1,142 +1,213 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
-const TaxPayerThree = ({nextStep, prevStep}) => {
-    const [taxpayerData, setTaxpayerData] = useState({
-        stationID: "",
-        consultantID: "",
-        stateID: "",
-        lgaID: "",
-        arithOrder: ""
-    });
+const TaxPayerThree = ({ nextStep, prevStep, setOnboardingData }) => {
+  const user = JSON.parse(localStorage.getItem('user'))
+  const [taxpayerData, setTaxpayerData] = useState({
+    miniStationId: '',
+    stateID: '',
+    localGovernmentId: '',
+    taxId: '',
+  })
 
-    const [taxpayerId, setTaxpayerId] = useState('')
+  const [isFormComplete, setIsFormComplete] = useState(false)
+  const [showError, setShowError] = useState(false)
+  const [miniTaxStations, setMiniTaxStations] = useState(null)
+  const [stateData, setStateData] = useState(null)
+  const [lgaData, setLgaData] = useState(null)
 
-    const [isFormComplete, setIsFormComplete] = useState(false);
-
-    const handleChange = (e) => {
-        const {name, value} = e.target
-        setTaxpayerData((prevData) => ({
-            ...prevData, [name] : value
-        }))
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await Promise.all([
+          axios('https://assettrack.com.ng/api/MiniTaxStation'),
+          axios('https://assettrack.com.ng/api/State'),
+        ])
+        setMiniTaxStations(data[0].data)
+        setStateData(data[1].data)
+      } catch (error) {
+        console.error('Error fetching data', error)
+      }
     }
 
-    useEffect(() => {
-      const checkFormComplete = () => {
-        const isComplete = Object.values(taxpayerData).every(value => value.trim() !== "");
-            setIsFormComplete(isComplete)
-        }   
+    fetchData()
+  }, [])
 
-        checkFormComplete()
-    }, [taxpayerData]);
+  useEffect(() => {
+    const fetchLgaData = async () => {
+      try {
+        let response
+        if (taxpayerData.stateID)
+          response = await axios.get(
+            `https://assettrack.com.ng/api/lgas/bystate/${taxpayerData.stateID}`,
+          )
+        setLgaData(response?.data)
+      } catch (error) {
+        console.error('Error fetching LGA Data', error)
+      }
+    }
 
-    const {stationID,consultantID,stateID,lgaID,arithOrder} = taxpayerData;
+    fetchLgaData()
+  }, [taxpayerData.stateID])
 
-    useEffect(() => {
-        const getTaxpayerID = async() => {
-            try {
-                const response = await axios.get(`http://assettrack.com.ng/api/Generator/NewTaxPayerId/${stationID},${consultantID},${stateID},${lgaID},${arithOrder}`)
-                setTaxpayerId(response.data)
-            } catch (error) {
-                console.error("Error getting tax payer ID")
-            }
-        }
+  const handleChange = e => {
+    const { name, value } = e.target
+    setTaxpayerData(prevData => ({
+      ...prevData,
+      [name]: value,
+    }))
+  }
 
-        if (isFormComplete) {
-            getTaxpayerID()
-        }
-        
-    },[isFormComplete])
-    
+  useEffect(() => {
+    const checkFormComplete = () => {
+      const isComplete = Object.values(taxpayerData).every(
+        value => value.trim() !== '',
+      )
+      setIsFormComplete(isComplete)
+    }
 
-    return ( 
-        <div className="font-montserrat flex flex-col items-center">
-             <div className="flex flex-col text-center mt-16 mb-8">
-                 <h2 className="font-bold text-3xl">Tax Payer Registration</h2>
-                 <p className="font-normal text-2xl">Confirm all required information for registration</p>
-             </div>
-             <div className="w-3/5">
-                 <form className="flex flex-col items-center">
-                    <div className="w-full flex flex-col pb-5">
-                        <label>
-                            Station ID
-                        </label>
-                        <input 
-                         type="text"
-                         name="stationID" 
-                         value={stationID}
-                         onChange={handleChange}
-                         placeholder="Enter Station ID"
-                         className="border-2 border-tax-blue py-4 px-5 outline-none placeholder:text-gray-300 rounded"/>
-                    </div>
-                    <div className="w-full flex flex-col pb-5">
-                        <label>
-                            Consultant ID
-                        </label>
-                        <input 
-                         type="text"
-                         name="consultantID"
-                         value={consultantID}
-                         onChange={handleChange}
-                         placeholder="Enter Consultant ID"
-                         className="border-2 border-tax-blue py-4 px-5 outline-none placeholder:text-gray-300 rounded"/>
-                    </div>
-                    <div className="w-full flex flex-col pb-5">
-                        <label>
-                            State ID
-                        </label>
-                        <input 
-                         type="text"
-                         name="stateID"
-                         value={stateID}
-                         onChange={handleChange}
-                         placeholder="Enter State ID"
-                         className="border-2 border-tax-blue py-4 px-5 outline-none placeholder:text-gray-400 rounded"/>
-                    </div>
-                    <div className="w-full flex flex-col pb-5">
-                        <label>
-                            LGA ID 
-                        </label>
-                        <input 
-                        type="text"
-                        name="lgaID"
-                        value={lgaID}
-                        onChange={handleChange}
-                        placeholder="Enter Local Government ID"
-                        className="border-2 border-tax-blue py-4 px-5 outline-none placeholder:text-gray-400 rounded"/>
-                    </div>
-                    <div className="w-full flex flex-col pb-5">
-                        <label>
-                            Arith Order
-                        </label>
-                        <input 
-                        type="text"
-                        name="arithOrder"
-                        value={arithOrder}
-                        onChange={handleChange}
-                        placeholder="Enter Arith Order"
-                        className="border-2 border-tax-blue py-4 px-5 outline-none placeholder:text-gray-400 rounded"/>
-                    </div>
-                    <div className="w-full flex flex-col  pb-10">
-                        <label>
-                            Tax Payer ID
-                        </label>
-                        <p className="border-2 border-tax-blue py-4 px-5 outline-none placeholder:text-gray-400 rounded">
-                            {taxpayerId ? taxpayerId : "0000000"}
-                        </p>
-                    </div>
-                     <div className="w-full pb-5 flex gap-12">
-                         <button className="bg-[#CED8F2] w-full py-3 text-tax-blue rounded-md text-2xl" onClick={prevStep}>
-                             Back
-                         </button>
-                         <button className="bg-tax-blue w-full py-3 text-white rounded-md text-2xl" onClick={nextStep}>
-                             Next
-                         </button>
-                     </div>
-                 </form>
-             </div>
-         </div>
-     );
+    checkFormComplete()
+  }, [taxpayerData])
+
+  const handleNextStep = (e) => {
+    e.preventDefault()
+    if (isFormComplete) {
+      setOnboardingData(prevData => ({
+        ...prevData,
+        ...taxpayerData,
+      }))
+      nextStep()
+    } else {
+      setShowError(true)
+    }
+  }
+
+  const { miniStationId, stateID, localGovernmentId, taxId } = taxpayerData
+
+  useEffect(() => {
+    const getTaxpayerID = async () => {
+      try {
+        const response = await axios.get(
+          `https://assettrack.com.ng/api/Generator/NewTaxPayerId/${miniStationId},${
+            user.email
+          },${stateID},${localGovernmentId},${Math.floor(Math.random() * 5 + 1)}`,
+        )
+        setTaxpayerData(prevData => ({
+          ...prevData,
+          taxId: response.data?.taxId,
+        }))
+      } catch (error) {
+        console.error('Error getting tax payer ID')
+      }
+    }
+
+    if ((miniStationId && stateID && localGovernmentId && user.email)) {
+      getTaxpayerID()
+    }
+  }, [miniStationId, stateID, localGovernmentId, user.email])
+
+  return (
+    <div className='font-montserrat flex flex-col items-center'>
+      <div className='flex flex-col text-center mt-16 mb-8'>
+        <h2 className='font-bold text-3xl'>Tax Payer Registration</h2>
+        <p className='font-normal text-2xl'>
+          Confirm all required information for registration
+        </p>
+      </div>
+      <div className='w-3/5'>
+        <form className='flex flex-col items-center'>
+          <div className='w-full flex gap-16 pb-5'>
+            <div className='w-full flex flex-col bg-white'>
+              <label>Mini Tax Station</label>
+              <select
+                name='miniStationId'
+                value={miniStationId}
+                onChange={handleChange}
+                className='border-2 border-tax-blue py-4 px-5 outline-none rounded text-gray-400 bg-white'
+                disabled={!miniTaxStations}
+              >
+                <option value='' key='select'>
+                  Select Mini Tax Station
+                </option>
+                {miniTaxStations?.map(({ name, id }) => (
+                  <option className='text-black' value={id} key={id}>
+                    {name}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+          <div className='w-full flex gap-16 pb-5'>
+            <div className='w-full flex flex-col'>
+              <label>State</label>
+              <select
+                name='stateID'
+                value={stateID}
+                onChange={handleChange}
+                className='border-2 border-tax-blue py-4 px-5 outline-none rounded text-gray-400 bg-white'
+                disabled={!stateData}
+              >
+                <option value='' key='select'>
+                  Select State
+                </option>
+                {stateData?.map(({ stateId, stateName }) => (
+                  <option className='text-black' value={stateId} key={stateId}>
+                    {stateName}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+          <div className='w-full flex gap-16 pb-5'>
+            <div className='w-full flex flex-col'>
+              <label>LGA</label>
+              <select
+                name='localGovernmentId'
+                value={localGovernmentId}
+                onChange={handleChange}
+                disabled={!stateID}
+                className='border-2 border-tax-blue py-4 px-5 outline-none rounded text-gray-400 bg-white'
+              >
+                <option value='' key='select'>
+                  Select LGA
+                </option>
+                {lgaData?.map(({ lgaId, lgaName }) => (
+                  <option className='text-black' value={lgaId} key={lgaId}>
+                    {lgaName}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+          <div className='w-full flex flex-col  pb-10'>
+            <label>Tax Payer ID</label>
+            <p className='border-2 border-tax-blue py-4 px-5 outline-none placeholder:text-gray-400 rounded'>
+              {taxId ? taxId : '0000000'}
+            </p>
+          </div>
+          {showError && (
+            <p className='text-red-500 text-sm text-center'>
+              Please fill in all fields
+            </p>
+          )}
+          <div className='w-full pb-5 flex gap-12'>
+            <button
+              className='bg-[#CED8F2] w-full py-3 text-tax-blue rounded-md text-2xl'
+              onClick={prevStep}
+            >
+              Back
+            </button>
+            <button
+              className='bg-tax-blue w-full py-3 text-white rounded-md text-2xl'
+              onClick={handleNextStep}
+            >
+              Next
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
 }
- 
-export default TaxPayerThree;
+
+export default TaxPayerThree
