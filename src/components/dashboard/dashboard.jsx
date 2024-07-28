e8730bf72c43f9a0141a2908bf7a5b82a648af03
Fawaz Adeniji <fawazadeniji123@gmail.com>
2024-07-28 19:05:24 +0100

    refactor: Update Dashboard component to fetch and display dashboard data
    The commit updates the Dashboard component to fetch and display dashboard data based on the user's role. It adds a useEffect hook to make an API request and retrieve the appropriate data based on the user's role. The fetched data is then used to populate the metrics displayed on the dashboard. This improvement enhances the application by providing real-time and role-specific data to the users, improving their experience and decision-making.

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -7,10 +7,44 @@ import { useNavigate } from 'react-router-dom'
 import TimeAndDate from '../time-and-date/time-and-date'
 import { useAuth } from '../../context/AuthContext'
 import { consultantInventoryData } from '../../data/consultantInventoryData'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { formatNumber } from '../../lib/formatNumber'
 
 const Dashboard = () => {
+  const [dashboardData, setDashboardData] = useState(null)
+  const [loading, setLoading] = useState(true)
   const navigate = useNavigate()
   const { user } = useAuth()
+
+  useEffect(() => {
+    async function fetchDashboardData() {
+      try {
+        setLoading(true)
+        switch (user.role) {
+          case 'admin':
+            const response = await axios.get(
+              'https://assettrack.com.ng/api/DashBoard',
+            )
+            setDashboardData(response.data)
+            break
+          case 'consultant':
+            const consultantResponse = await axios.get(
+              `https://assettrack.com.ng/api/DashBoard/ByConsulantEmail/${user.email}`,
+            )
+            setDashboardData(consultantResponse.data)
+            break
+          default:
+            break
+        }
+      } catch (error) {
+        console.log(error)
+      } finally {
+        setLoading(false)
+      }
+    }
+    fetchDashboardData()
+  }, [user.role, user.email])
 
   const addReg = reg => {
     navigate(`/dashboard/${reg}`)
@@ -19,17 +53,24 @@ const Dashboard = () => {
   return (
     <div className='p-4 w-full font-manrope'>
       <h2 className='font-normal font-manrope text-2xl mb-4'>
-        Welcome back, <span className='font-semibold capitalize'>{user.name}</span> ☀️
+        Welcome back,{' '}
+        <span className='font-semibold capitalize'>{user.name}</span> ☀️
       </h2>
 
       <div className='w-full grid grid-cols-4 mb-3 items-center gap-6'>
         {(user.role === 'admin' ? inventoryData : consultantInventoryData)
           .slice(0, -2)
-          .map(({ title, numbers, imageSrc }, index) => (
+          .map(({ title, imageSrc, accessor }, index) => (
             <Inventories
               key={index}
               title={title}
-              metric={numbers}
+              metric={
+                loading ? (
+                  <p className='text-sm'>loading...</p>
+                ) : (
+                  formatNumber(dashboardData[accessor]) ?? 'N/A'
+                )
+              }
               image={imageSrc}
             />
           ))}
@@ -49,30 +90,38 @@ const Dashboard = () => {
         <div className='w-full col-span-3 space-y-5'>
           {(user.role === 'admin' ? inventoryData : consultantInventoryData)
             .slice(-2)
-            .map(({ title, numbers, imageSrc }, index) => (
+            .map(({ title, imageSrc, accessor }, index) => (
               <Inventories
                 key={index}
                 title={title}
-                metric={numbers}
+                metric={
+                  loading ? (
+                    <p className='text-sm'>loading...</p>
+                  ) : (
+                    formatNumber(dashboardData[accessor]) ?? 'N/A'
+                  )
+                }
                 image={imageSrc}
               />
             ))}
 
-          <div className="w-full rounded-xl space-y-5 shadow p-4 flex flex-col justify-center items-center">
-            { user.role === "admin" && (
+          <div className='w-full rounded-xl space-y-5 shadow p-4 flex flex-col justify-center items-center'>
+            {user.role === 'admin' && (
               <button
-              className="w-[80%] text-white rounded-full gap-x-2 bg-tax-blue flex items-center justify-center px-5 py-3 my-16"
-              onClick={() => addReg("taxconsultant-registration")}>
-                <AiOutlinePlus className="text-2xl" />
-                <p className="text-sm">Add New Consultant</p>
+                className='w-[80%] text-white rounded-full gap-x-2 bg-tax-blue flex items-center justify-center px-5 py-3 my-16'
+                onClick={() => addReg('taxconsultant-registration')}
+              >
+                <AiOutlinePlus className='text-2xl' />
+                <p className='text-sm'>Add New Consultant</p>
               </button>
             )}
-            { user.role === "consultant" && (
+            {user.role === 'consultant' && (
               <button
-              className="w-[80%] text-white rounded-full gap-x-2 bg-tax-blue flex items-center justify-center px-5 py-3 my-16"
-              onClick={() => addReg("taxagent-registration")}>
-                <AiOutlinePlus className="text-2xl" />
-                <p className="text-sm">Add New Agent</p>
+                className='w-[80%] text-white rounded-full gap-x-2 bg-tax-blue flex items-center justify-center px-5 py-3 my-16'
+                onClick={() => addReg('taxagent-registration')}
+              >
+                <AiOutlinePlus className='text-2xl' />
+                <p className='text-sm'>Add New Agent</p>
               </button>
             )}
           </div>
