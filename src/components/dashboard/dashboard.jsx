73c2c57249f114c1e4914f12ed9ae87026089586
Fawaz Adeniji <fawazadeniji123@gmail.com>
2024-08-09 07:07:07 +0100

    refactor: Update Dashboard component to improve code readability and consistency

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -57,14 +57,14 @@ const Dashboard = () => {
   };
 
   return (
-    <div className="p-4 w-full font-manrope">
-      <h2 className="font-normal font-manrope text-2xl mb-4">
-        Welcome back,{" "}
-        <span className="font-semibold capitalize">{user.name}</span> ☀️
+    <div className='p-4 w-full font-manrope'>
+      <h2 className='font-normal font-manrope text-2xl mb-4'>
+        Welcome back,{' '}
+        <span className='font-semibold capitalize'>{user.name}</span> ☀️
       </h2>
 
-      <div className="w-full grid grid-cols-4 mb-3 items-center gap-6">
-        {(user.role === "admin" ? inventoryData : consultantInventoryData)
+      <div className='w-full grid grid-cols-4 mb-3 items-center gap-6'>
+        {(user.role === 'admin' ? inventoryData : consultantInventoryData)
           ?.slice(0, -2)
           ?.map(({ title, imageSrc, accessor }, index) => (
             <Inventories
@@ -72,29 +72,29 @@ const Dashboard = () => {
               title={title}
               metric={
                 loading ? (
-                  <p className="text-sm">loading...</p>
+                  <p className='text-sm'>loading...</p>
                 ) : (
-                  (formatNumber(dashboardData[accessor]) ?? "N/A")
+                  formatNumber(dashboardData[accessor]) ?? 'N/A'
                 )
               }
               image={imageSrc}
             />
           ))}
       </div>
-      <div className="w-full grid grid-cols-12 gap-6">
-        <div className="w-full col-span-9">
+      <div className='w-full grid grid-cols-12 gap-6'>
+        <div className='w-full col-span-9'>
           {/** */}
-          <div className="w-full grid grid-cols-5 ">
+          <div className='w-full grid grid-cols-5 '>
             <LineChart />
           </div>
           {/** */}
-          <div className="w-full mt-3 grid grid-cols-5 gap-3 items-start">
+          <div className='w-full mt-3 grid grid-cols-5 gap-3 items-start'>
             <TaxInflow />
           </div>
         </div>
         {/** 2 */}
-        <div className="w-full col-span-3 space-y-5">
-          {(user.role === "admin" ? inventoryData : consultantInventoryData)
+        <div className='w-full col-span-3 space-y-5'>
+          {(user.role === 'admin' ? inventoryData : consultantInventoryData)
             ?.slice(-2)
             ?.map(({ title, imageSrc, accessor }, index) => (
               <Inventories
@@ -102,32 +102,41 @@ const Dashboard = () => {
                 title={title}
                 metric={
                   loading ? (
-                    <p className="text-sm">loading...</p>
+                    <p className='text-sm'>loading...</p>
                   ) : (
-                    (formatNumber(dashboardData[accessor]) ?? "N/A")
+                    formatNumber(dashboardData[accessor]) ?? 'N/A'
                   )
                 }
                 image={imageSrc}
               />
             ))}
 
-          <div className="w-full rounded-xl space-y-5 shadow p-4 flex flex-col justify-center items-center">
-            {user.role === "admin" && (
+          <div className='w-full rounded-xl space-y-5 shadow p-4 flex flex-col justify-center items-center'>
+            {user.role === 'admin' && (
               <button
-                className="w-[80%] text-white rounded-full gap-x-2 bg-tax-blue flex items-center justify-center px-5 py-3 my-16"
-                onClick={() => addReg("taxconsultant-registration")}
+                className='w-[80%] text-white rounded-full gap-x-2 bg-tax-blue flex items-center justify-center px-5 py-3 my-16'
+                onClick={() => addReg('taxconsultant-registration')}
               >
-                <AiOutlinePlus className="text-2xl" />
-                <p className="text-sm">Add New Consultant</p>
+                <AiOutlinePlus className='text-2xl' />
+                <p className='text-sm'>Add New Consultant</p>
               </button>
             )}
-            {user.role === "consultant" && (
+            {user.role === 'consultant' && (
               <button
-                className="w-[80%] text-white rounded-full gap-x-2 bg-tax-blue flex items-center justify-center px-5 py-3 my-16"
-                onClick={() => addReg("taxagent-registration")}
+                className='w-[80%] text-white rounded-full gap-x-2 bg-tax-blue flex items-center justify-center px-5 py-3 my-16'
+                onClick={() => addReg('taxagent-registration')}
               >
-                <AiOutlinePlus className="text-2xl" />
-                <p className="text-sm">Add New Agent</p>
+                <AiOutlinePlus className='text-2xl' />
+                <p className='text-sm'>Add New Agent</p>
+              </button>
+            )}
+            {user.role === 'agent' && (
+              <button
+                className='w-[80%] text-white rounded-full gap-x-2 bg-tax-blue flex items-center justify-center px-5 py-3 my-16'
+                onClick={() => addReg('onbaording')}
+              >
+                <AiOutlinePlus className='text-2xl' />
+                <p className='text-sm'>Add Tax Payer</p>
               </button>
             )}
           </div>
@@ -138,7 +147,7 @@ const Dashboard = () => {
         </div>
       </div>
     </div>
-  );
+  )
 };
 
 export default Dashboard;
