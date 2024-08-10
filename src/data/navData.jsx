bef1361ed49c723899c12ee2ca16e803f6d032bb
Fawaz Adeniji <fawazadeniji123@gmail.com>
2024-08-10 19:57:48 +0100

    refactor: Update navData.jsx to use single quotes for string literals and remove unnecessary imports

--- src/data/navData.jsx
+++ src/data/navData.jsx
@@ -1,57 +1,57 @@
-import dashicon from "../assets/img/category.svg";
-import profile from "../assets/img/profile.svg";
-import receipt from "../assets/img/receipt-2.svg";
-import settings from "../assets/img/setting.svg";
-import logout from "../assets/img/logout.svg";
+import dashicon from '../assets/img/category.svg'
+import profile from '../assets/img/profile.svg'
+import receipt from '../assets/img/receipt-2.svg'
+import settings from '../assets/img/setting.svg'
+import logout from '../assets/img/logout.svg'
 
 export const navData = [
   {
-    label: "Dashboard",
+    label: 'Dashboard',
     icon: dashicon,
-    path: "/dashboard",
-    visible: ["admin", "consultant", "agent"],
+    path: '/dashboard',
+    visible: ['admin', 'consultant', 'agent'],
   },
   {
-    label: "Business",
+    label: 'Business',
     icon: profile,
-    path: "/dashboard/business/sector",
+    path: '/dashboard/business/sector',
     subsection: [
       {
-        label: "Sector Management",
+        label: 'Sector Management',
         icon: profile,
-        path: "/dashboard/business/sector",
+        path: '/dashboard/business/sector',
       },
       {
-        label: "Type Management",
+        label: 'Type Management',
         icon: profile,
-        path: "/dashboard/business/type",
+        path: '/dashboard/business/type',
       },
     ],
-    visible: ["admin"],
+    visible: ['admin'],
   },
   {
-    label: "Station Management",
+    label: 'Station Management',
     icon: receipt,
-    path: "/dashboard/station",
-    visible: ["admin"],
+    path: '/dashboard/station',
+    visible: ['admin'],
   },
   {
-    label: "Consultant",
+    label: 'Consultant',
     icon: receipt,
-    path: "/dashboard/consultant",
-    visible: ["admin"],
+    path: '/dashboard/consultant',
+    visible: ['admin'],
   },
   {
-    label: "Agent",
+    label: 'Agent',
     icon: receipt,
-    path: "/dashboard/agent",
-    visible: ["admin", "consultant"],
+    path: '/dashboard/agent',
+    visible: ['admin', 'consultant'],
   },
   {
-    label: "Tax Payer Management",
+    label: 'Tax Payer Management',
     icon: receipt,
-    path: "/dashboard/taxpayer",
-    visible: ["admin", "consultant", "agent"],
+    path: '/dashboard/taxpayer',
+    visible: ['admin', 'consultant', 'agent'],
   },
   // {
   //   label: "Tax Card Management",
@@ -60,39 +60,39 @@ export const navData = [
   //   visible: ["admin", "consultant"],
   // },
   {
-    label: "Onbarding",
+    label: 'Onbarding',
     icon: receipt,
-    path: "/dashboard/onboarding",
-    visible: ["agent"],
+    path: '/dashboard/onboarding',
+    visible: ['agent'],
   },
   {
-    label: "Assessment",
+    label: 'Assessment',
     icon: receipt,
-    path: "/dashboard/assessment",
-    visible: ["agent"],
+    path: '/dashboard/assessment',
+    visible: ['agent'],
   },
   {
-    label: "Billing",
+    label: 'Billing',
     icon: receipt,
-    path: "/dashboard/billing",
-    visible: ["agent"],
+    path: '/dashboard/billing',
+    visible: ['agent'],
   },
+  // {
+  //   label: "Payment",
+  //   icon: receipt,
+  //   path: "/dashboard/payment",
+  //   visible: ["agent"],
+  // },
   {
-    label: "Payment",
-    icon: receipt,
-    path: "/dashboard/payment",
-    visible: ["agent"],
-  },
-  {
-    label: "Settings",
+    label: 'Settings',
     icon: settings,
-    path: "/dashboard/settings",
-    visible: ["admin", "consultant", "agent"],
+    path: '/dashboard/settings',
+    visible: ['admin', 'consultant', 'agent'],
   },
   {
-    label: "Logout",
+    label: 'Logout',
     icon: logout,
-    path: "/login",
-    visible: ["admin", "consultant", "agent"],
+    path: '/login',
+    visible: ['admin', 'consultant', 'agent'],
   },
-];
+]
