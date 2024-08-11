87c17e9a3c72712f2aeb62e6cb9516bee0db836e
Fawaz Adeniji <fawazadeniji123@gmail.com>
2024-08-11 13:40:22 +0100

    refactor: Update component to add tax agentin charge

--- src/components/profile/consultantprofile.jsx
+++ src/components/profile/consultantprofile.jsx
@@ -2,10 +2,12 @@ import userprofile from '../../assets/img/face_placeholder.png'
 import phone from '../../assets/img/ph_phone-thin.png'
 import locationImg from '../../assets/img/system-uicons_location.png'
 import { AiOutlinePlus } from 'react-icons/ai'
-import PayerCard from './payercard/payercard'
-import { useNavigate, useParams } from 'react-router-dom'
-import { useEffect, useState } from 'react'
+import { Navigate, useNavigate, useParams } from 'react-router-dom'
+import { useEffect, useMemo, useState } from 'react'
 import axios from 'axios'
+import DataTable from '../data-table/data-table'
+import { Button } from '../ui/button'
+import { useAuth } from '../../context/AuthContext'
 
 const NGN = new Intl.NumberFormat('en-NG', {
   style: 'currency',
@@ -14,12 +16,89 @@ const NGN = new Intl.NumberFormat('en-NG', {
 
 const ConsultantProfile = () => {
   const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [loadingAgents, setLoadingAgents] = useState(true)
+  const [agentData, setAgentData] = useState([])
+
   const navigate = useNavigate()
   const { email } = useParams()
+  const { user } = useAuth()
+
+  const columns = useMemo(
+    () => [
+      {
+        header: 'Name',
+        accessorKey: 'name',
+        cell: ({ row }) => (
+          <div className='flex items-center gap-3'>
+            {`${row.original.firstName} ${row.original.lastName}`}
+          </div>
+        ),
+      },
+      {
+        header: 'Email Address',
+        accessorKey: 'emailAddress',
+        cell: ({ cell }) => (
+          <div className='flex items-center gap-3'>{cell.getValue()}</div>
+        ),
+      },
+      {
+        header: 'Address',
+        accessorKey: 'address',
+        cell: ({ cell }) => (
+          <div className='flex items-center gap-3'>{cell.getValue()}</div>
+        ),
+      },
+      {
+        header: 'Telephone',
+        accessorKey: 'telephone',
+        cell: ({ cell }) => (
+          <div className='flex items-center gap-3'>{cell.getValue()}</div>
+        ),
+      },
+      {
+        accessorKey: 'action',
+        header: '',
+        cell: ({ row }) => (
+          <div className='flex place-content-end pr-4'>
+            <Button
+              text='Profile'
+              handleButton={e => {
+                e.preventDefault()
+                navigate(
+                  `/dashboard/agent-profile/${row.original.emailAddress}`,
+                )
+              }}
+            />
+          </div>
+        ),
+      },
+    ],
+    [navigate],
+  )
+
+  useEffect(() => {
+    async function fetchAgentData() {
+      try {
+        setLoadingAgents(true)
+        const res = await axios.get(
+          `https://assettrack.com.ng/api/Agent/AllByConsult/${email}`,
+        )
+        setAgentData(res.data)
+      } catch (error) {
+        console.error(error)
+      } finally {
+        setLoadingAgents(false)
+      }
+    }
+
+    fetchAgentData()
+  }, [email])
 
   useEffect(() => {
     async function fetchData() {
       try {
+        setLoading(true)
         const response = await axios.get(
           `https://assettrack.com.ng/api/Consultant/ConsultantByEmail/${email}`,
         )
@@ -27,10 +106,19 @@ const ConsultantProfile = () => {
       } catch (error) {
         console.error(error)
         navigate('/dashboard/consultant')
+      } finally {
+        setLoading(false)
       }
     }
     fetchData()
   }, [email, navigate])
+
+  if (
+    user.role.toLowerCase() === 'consultant' &&
+    user.email.toLowerCase() !== email.toLowerCase()
+  ) {
+    return <Navigate to={`/dashboard/consultant-profile/${user.email}`} />
+  }
 
   return (
     <div className='w-full p-6 h-full space-y-6'>
@@ -39,38 +127,44 @@ const ConsultantProfile = () => {
         <div className='w-full flex items-center justify-around h-full col-span-8'>
           <div className='text-[#4C4C4C] flex flex-col gap-y-10 items-start justify-start border-r p-2 pr-14'>
             <div className='flex flex-col items-start justify-start gap-y-1'>
-              <p className='font-medium text-lg'>{data?.companyName}</p>
+              <p className='font-medium text-lg'>
+                {!loading ? data?.companyName : '...'}
+              </p>
               <p className='text-base'>Consultant</p>
             </div>
             <div className='flex flex-col items-start justify-start gap-y-1'>
               <p className='text-base flex items-center gap-x-2'>
                 <img src={phone} alt='' />
-                <span>{data?.telephoneNo}</span>
+                <span>{!loading ? data?.telephoneNo : '...'}</span>
               </p>
               <p className='text-base flex items-center gap-x-2'>
                 <img src={locationImg} alt='' />
-                <span>Lagos</span>
+                <span>N/A</span>
               </p>
             </div>
           </div>
           <div className='text-[#4C4C4C] flex flex-col gap-y-10 items-start justify-start border-r p-2 pr-24'>
             <div className='flex flex-col items-start justify-start gap-y-1'>
               <p className='font-medium text-lg'>Email</p>
-              <p className='text-base'>{data?.emailAddress}</p>
+              <p className='text-base'>
+                {!loading ? data?.emailAddress : '...'}
+              </p>
             </div>
             <div className='flex flex-col items-start justify-start gap-y-1'>
               <p className='font-medium text-lg'>Consultant Code</p>
-              <p className='text-base'>{data?.consultantCode}</p>
+              <p className='text-base'>
+                {!loading ? data?.consultantCode : '...'}
+              </p>
             </div>
           </div>
           <div className='text-[#4C4C4C] flex flex-col gap-y-10 items-start justify-start p-2'>
             <div className='flex flex-col items-start justify-start gap-y-1'>
               <p className='font-medium text-lg'>Address</p>
-              <p className='text-base'>{data?.address}</p>
+              <p className='text-base'>{!loading ? data?.address : '...'}</p>
             </div>
             <div className='flex flex-col items-start justify-start gap-y-1'>
               <p className='font-medium text-lg'>Tax Station</p>
-              <p className='text-base'>Ikeja, Lagos</p>
+              <p className='text-base'>N/A</p>
             </div>
           </div>
         </div>
@@ -79,13 +173,13 @@ const ConsultantProfile = () => {
       <div className='w-full flex gap-6 items-center'>
         <div className='rounded-lg p-4 gap-3 border border-[#87BB42] flex flex-col items-start justify-start min-w-48 h-fit'>
           <p className='font-semibold text-xl'>
-            {NGN.format(data?.wallet?.balance)}
+            {!loading ? NGN.format(data?.wallet?.balance) : '...'}
           </p>
           <p className='text-base'>Balance</p>
         </div>
         <div className='rounded-lg p-4 gap-3 border border-[#938406] flex flex-col items-start justify-start min-w-48 h-fit'>
           <p className='font-semibold text-xl'>
-            {NGN.format(data?.amountDeposited)}
+            {!loading ? NGN.format(data?.amountDeposited) : '...'}
           </p>
           <p className='text-base'>Income</p>
         </div>
@@ -99,17 +193,14 @@ const ConsultantProfile = () => {
         </div>
       </div>
 
-      <div className='flex flex-col items-start justify-start gap-y-3'>
-        <h2 className='font-medium text-xl'>Tax Agent in Charge</h2>
+      <div className='flex flex-col items-start justify-start'>
+        <h2 className='font-medium text-xl'>Agents in charge</h2>
         <div className='w-full'>
-          <div className='w-full p-2 text-sm bg-[#F7F7F7] grid grid-cols-5'>
-            <p className='col-span-2'>Payer</p>
-            <p>Status</p>
-            <p>Tax Station</p>
-            <p>Action</p>
-          </div>
-          <PayerCard name={'Anuoluwapo'} station={'Lagos'} status={'Pending'} />
-          <PayerCard name={'Olabode'} station={'Lagos'} status={'Verified'} />
+          <DataTable
+            data={agentData}
+            columns={columns}
+            loading={loadingAgents}
+          />
         </div>
       </div>
     </div>
