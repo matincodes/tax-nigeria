bfab80d7bc7e12d94d8db81e8c1058812b05eecd
Fawaz Adeniji <fawazadeniji123@gmail.com>
2024-08-15 06:57:21 +0100

    refactor: Remove commented-out code in Dashboard component

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -169,17 +169,3 @@ const Dashboard = () => {
 
 export default Dashboard
 
-/**
- import { Route, Routes } from "react-router-dom";
-import DashNav from "../../routes/dashnav/dashnav";
-
-const Dashboard = () => {
-    return ( 
-        <Routes>
-            <Route path="/" element={<DashNav/> } />
-        </Routes>
-     );
-}
- 
-export default Dashboard;
- */
